f80fa974e041befb33429315ce9c17b20438b449
mjpeake <peake.matthew@gmail.com>
2025-02-15 23:21:57 +0000

    Adding ability to use non-canvas element

--- flowfields/flowfields.ts
+++ flowfields/flowfields.ts
@@ -4,18 +4,28 @@ import Examples from "./fields/examples";
 
 function FlowField(id: string = "flowfield", config: Config): void {
     // Append the application canvas to the document body
-    const canvas = <HTMLCanvasElement> document.getElementById(id);
-    if (!canvas) {
+    let container = <HTMLCanvasElement>document.getElementById(id);
+    if (!container) {
         console.error(`element with id "${id}" not found.`);
         return;
     }
-    const ctx = canvas.getContext("2d");
-    if (!ctx) {
-        console.error(`element with id "${id}" is not a canvas.`);
-        return
+    let canvas: HTMLCanvasElement;
+    if (container instanceof HTMLCanvasElement) {
+        canvas = container
+    } else {
+        canvas = loadCanvas(container);
     }
     console.log("using config", config)
     Draw(canvas, config);
+}
+
+function loadCanvas(div: HTMLElement): HTMLCanvasElement {
+    var canvas = <HTMLCanvasElement>document.createElement('canvas')
+    canvas.id = "flowfields-canvas";
+    canvas.style.width = "100%";
+    canvas.style.height = "100%";
+    div.appendChild(canvas);
+    return canvas;
 }
 
 export {
